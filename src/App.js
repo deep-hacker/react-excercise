662a73c1178e6bd8e00884edc8066856a822ad03
VIKASH, VIVEK <vivek.vikash@cj.com>
2021-03-30 10:58:33 +0530

    App.js modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, {useEffect, useState} from "react";
 import './App.css';
-import ProfileList from "./profileList";
-
+import DefaultTopLevel from "./Dependencies";
 
 const App = () => {
   return (
    <div className="App">
-     <ProfileList/>
+    <DefaultTopLevel/>
    </div>
   );
 };
